17745cf56f03ee242f449de20a572447936c2887
kev264FR <65603715+kev264FR@users.noreply.github.com>
2020-09-21 20:00:33 +0200

    Petit changement js

--- public/js/js.js
+++ public/js/js.js
@@ -2,4 +2,16 @@ $("#logo").click(function(){
     $("*").toggleClass("vibrate-1")
 })
 
+$.confirm.options = {
+    text: "Cette action est définitive, voulez vous continuer ?",
+    title: "",
+    confirmButton: "Oui",
+    cancelButton: "Non",
+    post: false,
+    submitForm: false,
+    confirmButtonClass: "btn-danger",
+    cancelButtonClass: "btn-info",
+    dialogClass: "modal-dialog"
+}
+
 $(".confirm").confirm()
